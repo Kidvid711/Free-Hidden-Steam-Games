9444db97fd85440d4c38c0b5de260c3d9f923a8b
Kidvid711 <132766911+Kidvid711@users.noreply.github.com>
2024-01-28 20:27:29 -0500

    Create ver1.bat

---
+++ ver1.bat
@@ -0,0 +1,107 @@
+Echo off
+echo Please wait 10 seconds after each install. The Steam Client is required in order for script to work. 
+pause
+Echo Installing Age of Empires Online
+Start Steam://install/105430
+timeout 11 
+pause
+Echo Installing America’s Army 3
+Start Steam://install/13140
+timeout 11 
+pause
+Echo InstallingArcane Saga Online
+Start Steam://install/238110
+timeout 11 
+pause
+Echo Installing Arctic Combat
+Start Steam://install/212370
+timeout 11 
+pause
+Echo Installing Arma 2
+Start Steam://install/107400
+timeout 11 
+pause
+Echo Installing Battle for Graxia
+Start Steam://install/90530
+timeout 11 
+pause
+Echo Installing Brawl Busters
+Start Steam://install/109410
+timeout 11 
+pause
+Echo Installing Bullet Run
+Start Steam://install/211880
+timeout 11 
+pause
+Echo Installing Codename Gordon
+Start Steam://install/92
+timeout 11 
+pause
+Echo Installing District 187
+Start Steam://install/221080
+timeout 11 
+pause
+Echo Installing Dungeon Fighter Online
+Start Steam://install/212220
+timeout 11 
+pause
+Echo Installing F.E.A.R. Online
+Start Steam://install/223650
+timeout 11 
+pause
+Echo Installing Fnaf world
+Start Steam://install/427920
+timeout 11 
+pause
+Echo Installing Haunted Memories
+Start Steam://install/241640
+timeout 11 
+pause
+Echo Installing Maple Story
+Start Steam://install/216150
+timeout 11 
+pause
+Echo Installing Pandora Saga
+Start Steam://install/106010
+timeout 11 
+pause
+Echo Installing Peggle Extreme
+Start Steam://install/3483
+timeout 11 
+pause
+Echo Installing Renaissance Heroes
+Start Steam://install/221790
+timeout 11 
+pause
+Echo Installing Rusty Hearts
+Start Steam://install/36630
+timeout 11 
+pause
+Echo Installing Smashball
+Start Steam://install/17730
+timeout 11 
+pause
+Echo Installing Spacewar
+Start Steam://install/480
+timeout 11 
+pause
+Echo Installing TERA
+Start Steam://install/389300
+timeout 11 
+pause
+Echo Installing TERA (EU)
+Start Steam://install/323370
+timeout 11 
+pause
+Echo Installing Trackmania Nations Forever
+Start Steam://install/11020
+timeout 11 
+pause
+Echo Installing Vanguard: Saga of Heroes F2P
+Start Steam://install/218210
+timeout 11 
+pause
+Echo Installing Wizardry Online
+Start Steam://install/221360
+
+Echo Thank you for using the script. It was my first time making it. 
